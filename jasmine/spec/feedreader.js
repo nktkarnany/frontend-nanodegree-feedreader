51763b0cc5ee1699dbbe897cef58cdc901123376
Ankit Karnany <ankit@greylabs.org>
2017-04-07 23:59:18 +0530

    new feed selection suite implemented

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -81,6 +81,8 @@ $(function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function() {
+
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -88,10 +90,39 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('Atleast one entry in the feed', function(done) {
+            expect($('.feed').length).toBeGreaterThan(0);
+            done();
+        });
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+        $('.feed').empty();
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('loadFeed function call changes data', function(done) {
+            expect($('.feed').length).toBeGreaterThan(0);
+            done();
+        });
+
+    });
+
 }());
